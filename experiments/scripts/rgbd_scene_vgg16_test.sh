432e91840a955eb8e582f71160bd759976b712b9
yuxng <xyfudan@gmail.com>
2016-12-16 11:46:30 -0800

    add test rgbd scene

--- experiments/scripts/rgbd_scene_vgg16_test.sh
+++ experiments/scripts/rgbd_scene_vgg16_test.sh
@@ -7,14 +7,14 @@ export PYTHONUNBUFFERED="True"
 export CUDA_VISIBLE_DEVICES=$1
 # export LD_PRELOAD=/usr/lib/libtcmalloc.so.4
 
-LOG="experiments/logs/gmu_scene_vgg16_test.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
+LOG="experiments/logs/rgbd_scene_vgg16_test.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
 exec &> >(tee -a "$LOG")
 echo Logging output to "$LOG"
 
 # test FCN for multiple frames
 time ./tools/test_net.py --gpu 0 \
   --network vgg16 \
-  --model output/gmu_scene/gmu_scene_train/vgg16_fcn_rgbd_multi_frame_gmu_scene_iter_20000.ckpt \
-  --imdb gmu_scene_val \
-  --cfg experiments/cfgs/gmu_scene.yml \
+  --model output/rgbd_scene/rgbd_scene_train/vgg16_fcn_rgbd_multi_frame_rgbd_scene_iter_25000.ckpt \
+  --imdb rgbd_scene_val \
+  --cfg experiments/cfgs/rgbd_scene.yml \
   --rig lib/kinect_fusion/data/camera.json
