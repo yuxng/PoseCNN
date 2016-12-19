afad75e01ed80d7b6f131daca18e513249840a19
yuxng <xyfudan@gmail.com>
2016-12-18 17:59:11 -0800

    ready to train shapenet scene

--- experiments/scripts/shapenet_scene_vgg16_depth_test.sh
+++ experiments/scripts/shapenet_scene_vgg16_depth_test.sh
@@ -5,7 +5,7 @@ set -e
 
 export PYTHONUNBUFFERED="True"
 export CUDA_VISIBLE_DEVICES=$1
-# export LD_PRELOAD=/usr/lib/libtcmalloc.so.4
+export LD_PRELOAD=/usr/lib/libtcmalloc.so.4
 
 LOG="experiments/logs/shapenet_scene_vgg16_depth_test.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
 exec &> >(tee -a "$LOG")
@@ -14,7 +14,7 @@ echo Logging output to "$LOG"
 # test FCN for multiple frames
 time ./tools/test_net.py --gpu 0 \
   --network vgg16 \
-  --model output/shapenet_scene/shapenet_scene_train/vgg16_fcn_depth_multi_frame_shapenet_scene_iter_20000.ckpt \
+  --model output/shapenet_scene/shapenet_scene_train/vgg16_fcn_depth_multi_frame_shapenet_scene_iter_40000.ckpt \
   --imdb shapenet_scene_val \
   --cfg experiments/cfgs/shapenet_scene.yml \
   --rig lib/kinect_fusion/data/camera.json
