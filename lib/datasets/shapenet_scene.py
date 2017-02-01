e5f73c81a1edafa7c3b2697e5b8b3edc65860e34
yuxng <xyfudan@gmail.com>
2017-02-01 12:52:51 -0800

    new shapenet dataset

--- lib/datasets/shapenet_scene.py
+++ lib/datasets/shapenet_scene.py
@@ -15,9 +15,9 @@ class shapenet_scene(datasets.imdb):
         self._shapenet_scene_path = self._get_default_path() if shapenet_scene_path is None \
                             else shapenet_scene_path
         self._data_path = os.path.join(self._shapenet_scene_path, 'data')
-        self._classes = ('__background__', 'table', 'tvmonitor', 'bottle', 'mug', 'bowl', 'can', 'keyboard', 'cap')
-        self._class_colors = [(0, 0, 0), (255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0), (255, 0, 255), (0, 255, 255), (188, 0, 0), (0, 188, 0)]
-        self._class_weights = [1, 1, 1, 1, 1, 1, 1, 1, 1]
+        self._classes = ('__background__', 'table', 'tvmonitor', 'bottle', 'mug', 'can', 'keyboard', 'cap')
+        self._class_colors = [(255, 255, 255), (255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0), (255, 0, 255), (0, 255, 255), (188, 0, 0)]
+        self._class_weights = [1, 1, 1, 1, 1, 1, 1, 1]
         self._class_to_ind = dict(zip(self.classes, xrange(self.num_classes)))
         self._image_ext = '.png'
         self._image_index = self._load_image_set_index()
@@ -260,7 +260,7 @@ class shapenet_scene(datasets.imdb):
             filename = os.path.join(mat_dir, '%04d.mat' % im_ind)
             print filename
             scipy.io.savemat(filename, labels)
-            """
+            #"""
 
         # overall accuracy
         acc = np.diag(hist).sum() / hist.sum()
