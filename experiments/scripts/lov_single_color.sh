f63354e4bc60fafbb90fdaf2be8a3a93ad0ae25f
yuxng <yuxiang@umich.edu>
2017-02-12 01:07:34 -0800

    new script

--- experiments/scripts/lov_single_color.sh
+++ experiments/scripts/lov_single_color.sh
@@ -17,17 +17,17 @@ time ./tools/train_net.py --gpu 0 \
   --weights data/imagenet_models/vgg16_convs.npy \
   --imdb lov_train \
   --cfg experiments/cfgs/lov_single_color.yml \
-  --iters 80000
+  --iters 40000
 
-if [ -f $PWD/output/lov/lov_val/vgg16_fcn_color_single_frame_lov_iter_80000/segmentations.pkl ]
+if [ -f $PWD/output/lov/lov_val/vgg16_fcn_color_single_frame_lov_iter_40000/segmentations.pkl ]
 then
-  rm $PWD/output/lov/lov_val/vgg16_fcn_color_single_frame_lov_iter_80000/segmentations.pkl
+  rm $PWD/output/lov/lov_val/vgg16_fcn_color_single_frame_lov_iter_40000/segmentations.pkl
 fi
 
 # test FCN for single frames
 time ./tools/test_net.py --gpu 0 \
   --network vgg16_convs \
-  --model output/lov/lov_train/vgg16_fcn_color_single_frame_lov_iter_80000.ckpt \
+  --model output/lov/lov_train/vgg16_fcn_color_single_frame_lov_iter_40000.ckpt \
   --imdb lov_val \
   --cfg experiments/cfgs/lov_single_color.yml \
   --rig data/LOV/camera.json
