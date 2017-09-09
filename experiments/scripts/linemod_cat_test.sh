b56fb21a8d1295f17b86f5e05e2d6d3c8661e4ef
yuxng <xyfudan@gmail.com>
2017-09-08 23:32:16 -0700

    duck

--- experiments/scripts/linemod_cat_test.sh
+++ experiments/scripts/linemod_cat_test.sh
@@ -23,7 +23,7 @@ echo Logging output to "$LOG"
 # test for pose regression
 time ./tools/test_net.py --gpu 0 \
   --network vgg16_convs \
-  --model output/linemod/linemod_cat_train/vgg16_fcn_color_single_frame_pose_linemod_cat_iter_40000.ckpt \
+  --model output/linemod/linemod_cat_train/vgg16_fcn_color_single_frame_pose_linemod_cat_iter_80000.ckpt \
   --imdb linemod_cat_test \
   --cfg experiments/cfgs/linemod_cat_pose.yml \
   --cad data/LINEMOD/models.txt \
