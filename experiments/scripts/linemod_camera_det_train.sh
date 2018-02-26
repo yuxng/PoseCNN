9f7f9205105c7510d4ca693453ea9d09f0259427
yuxng <xyfudan@gmail.com>
2018-02-25 18:59:37 -0800

    train linemod

---
+++ experiments/scripts/linemod_camera_det_train.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -x
+set -e
+
+export PYTHONUNBUFFERED="True"
+export CUDA_VISIBLE_DEVICES=$1
+
+LOG="experiments/logs/linemod_camera_det_train.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
+exec &> >(tee -a "$LOG")
+echo Logging output to "$LOG"
+
+export LD_PRELOAD=/usr/lib/libtcmalloc.so.4
+
+time ./tools/train_net.py --gpu 0 \
+  --network vgg16_det \
+  --weights data/imagenet_models/vgg16.npy \
+  --imdb linemod_camera_train_few \
+  --cfg experiments/cfgs/linemod_camera_det.yml \
+  --iters 160000
