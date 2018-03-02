b42e70ace014758c18b8e93f45d54e550e0f5920
yuxng <xyfudan@gmail.com>
2018-03-02 12:35:13 -0800

    new scirpt

--- experiments/scripts/lov_color_2d_train_full.sh
+++ experiments/scripts/lov_color_2d_train_full.sh
@@ -6,7 +6,7 @@ set -e
 export PYTHONUNBUFFERED="True"
 export CUDA_VISIBLE_DEVICES=$1
 
-LOG="experiments/logs/lov_color_2d_train.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
+LOG="experiments/logs/lov_color_2d_train_full.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
 exec &> >(tee -a "$LOG")
 echo Logging output to "$LOG"
 
@@ -14,10 +14,10 @@ echo Logging output to "$LOG"
 export LD_PRELOAD=/usr/lib/libtcmalloc.so.4
 
 time ./tools/train_net.py --gpu 0 \
-  --network vgg16_convs \
+  --network vgg16_full \
   --weights data/imagenet_models/vgg16.npy \
   --imdb lov_trainval \
-  --cfg experiments/cfgs/lov_color_2d.yml \
+  --cfg experiments/cfgs/lov_color_2d_full.yml \
   --cad data/LOV/models.txt \
   --pose data/LOV/poses.txt \
   --iters 120000
