7da230ff29cdb9127f70996349efe05b6fb7fe04
yuxng <xyfudan@gmail.com>
2018-03-02 12:20:14 -0800

    4 gpu

--- experiments/scripts/lov_color_2d_train.sh
+++ experiments/scripts/lov_color_2d_train.sh
@@ -13,19 +13,18 @@ echo Logging output to "$LOG"
 # train FCN for single frames
 export LD_PRELOAD=/usr/lib/libtcmalloc.so.4
 
-time ./tools/train_net.py --gpu 0 \
-  --num_gpus 2 \
-  --network vgg16_convs \
-  --weights data/imagenet_models/vgg16_convs.npy \
-  --imdb lov_trainval \
-  --cfg experiments/cfgs/lov_color_2d.yml \
-  --iters 20000
+#time ./tools/train_net.py --gpu 0 \
+#  --num_gpus 2 \
+#  --network vgg16_convs \
+#  --weights data/imagenet_models/vgg16_convs.npy \
+#  --imdb lov_trainval \
+#  --cfg experiments/cfgs/lov_color_2d.yml \
+#  --iters 20000
 
 time ./tools/train_net.py --gpu 0 \
-  --num_gpus 2 \
+  --num_gpus 4 \
   --network vgg16_convs \
   --weights data/imagenet_models/vgg16.npy \
-  --ckpt output/lov/lov_trainval/vgg16_fcn_color_single_frame_2d_lov_iter_20000.ckpt \
   --imdb lov_trainval \
   --cfg experiments/cfgs/lov_color_2d_pose.yml \
   --cad data/LOV/models.txt \
