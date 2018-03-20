636afff2577e8d488a7d363417176bc4a124d399
yuxng <xyfudan@gmail.com>
2018-03-20 10:21:28 -0700

    retrain

--- experiments/scripts/lov_color_2d_train.sh
+++ experiments/scripts/lov_color_2d_train.sh
@@ -16,7 +16,7 @@ export LD_PRELOAD=/usr/lib/libtcmalloc.so.4
 time ./tools/train_net.py --gpu 0 \
   --network vgg16_convs \
   --weights data/imagenet_models/vgg16.npy \
-  --imdb lov_train_few \
+  --imdb lov_train \
   --cfg experiments/cfgs/lov_color_2d.yml \
   --cad data/LOV/models.txt \
   --pose data/LOV/poses.txt \
