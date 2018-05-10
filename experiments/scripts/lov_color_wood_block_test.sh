70ffd78197e1f348ed952b2c4d991043a656c05e
yuxng <xyfudan@gmail.com>
2018-05-10 13:23:45 -0700

    add wood block

---
+++ experiments/scripts/lov_color_wood_block_test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -x
+set -e
+
+export PYTHONUNBUFFERED="True"
+export CUDA_VISIBLE_DEVICES=$1
+
+LOG="experiments/logs/lov_color_wood_block_test.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
+exec &> >(tee -a "$LOG")
+echo Logging output to "$LOG"
+
+# test FCN for single frames
+time ./tools/test_net.py --gpu 0 \
+  --network vgg16_convs \
+  --model output/lov/lov_036_wood_block_train/vgg16_fcn_color_single_frame_2d_pose_add_lov_wood_block_iter_160000.ckpt \
+  --imdb lov_single_036_wood_block_train \
+  --cfg experiments/cfgs/lov_color_wood_block.yml \
+  --rig data/LOV/camera.json \
+  --cad data/LOV/models.txt \
+  --pose data/LOV/poses.txt \
+  --background data/cache/backgrounds.pkl
---
+++ experiments/scripts/lov_color_wood_block_train.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -x
+set -e
+
+export PYTHONUNBUFFERED="True"
+export CUDA_VISIBLE_DEVICES=$1
+
+LOG="experiments/logs/lov_color_wood_block_train.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
+exec &> >(tee -a "$LOG")
+echo Logging output to "$LOG"
+
+# train FCN for single frames
+export LD_PRELOAD=/usr/lib/libtcmalloc.so.4
+
+time ./tools/train_net.py --gpu 0 \
+  --network vgg16_convs \
+  --weights data/imagenet_models/vgg16.npy \
+  --imdb lov_single_036_wood_block_train \
+  --cfg experiments/cfgs/lov_color_wood_block.yml \
+  --cad data/LOV/models.txt \
+  --pose data/LOV/poses.txt \
+  --iters 160000
