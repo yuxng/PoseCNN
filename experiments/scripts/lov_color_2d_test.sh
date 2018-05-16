6e7832b36383b5797a6f8ba83bdfeb8a82c2b3ff
yuxng <xyfudan@gmail.com>
2018-05-15 17:54:22 -0700

    no noise

--- experiments/scripts/lov_color_2d_test.sh
+++ experiments/scripts/lov_color_2d_test.sh
@@ -18,7 +18,7 @@ fi
 # test FCN for single frames
 time ./tools/test_net.py --gpu 0 \
   --network vgg16_convs \
-  --model output_napoli/lov/lov_train_few/vgg16_fcn_color_single_frame_2d_pose_nonsym_lov_iter_160000.ckpt \
+  --model output/lov/lov_train_few/vgg16_fcn_color_single_frame_2d_pose_sym_lov_iter_160000.ckpt \
   --imdb lov_val_few \
   --cfg experiments/cfgs/lov_color_2d.yml \
   --rig data/LOV/camera.json \
